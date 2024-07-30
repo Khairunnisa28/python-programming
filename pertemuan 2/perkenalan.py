118f6034696ac40b932555e64cab77b5730785dc
Nida Khairunnisa <138001140+Khairunnisa28@users.noreply.github.com>
2024-07-30 09:40:57 +0700

    Add files via upload

---
+++ pertemuan 2/perkenalan.py
@@ -0,0 +1,7 @@
+nama = "Nida Khairunnisa"
+kelas = "TKJ X A"
+hobi = "programming"
+
+print("Perkenalkan nama saya ", nama)
+print(f"Saya kelas {kelas}")
+print(f"hobi saya {hobi}, saya sangat senang melakukannya")
